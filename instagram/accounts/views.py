149d0a3f367a30ecc07eb4bc2f21ce3fa5805dd8
Jeonseoghyeon <jsh28592@gmail.com>
2020-04-17 10:41:01 +0000

    0417

---
+++ instagram/accounts/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
+# Create your views here.
+def index(request):
+    return render(request,'accounts/index.html')
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            auth_login(request,form.get_user())
+            return redirect('accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form':form
+        }
+    return render(request,'accounts/login.html',context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:index')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request,'accounts/welcome.html')
+    else:
+        form = UserCreationForm()
+    context = {
+        'form':form
+    }
+    return render(request,'accounts/signup.html',context)
