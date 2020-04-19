5bd6508dea13a874f0608cee8d3d91d32fce86f0
Jeonseoghyeon <jsh28592@gmail.com>
2020-04-19 12:26:58 +0000

    articles-urls.py 주석

--- instagram/articles/urls.py
+++ instagram/articles/urls.py
@@ -6,6 +6,7 @@ app_name = 'articles'
 urlpatterns = [
     path('',views.index,name='index'),
     path('create/',views.create,name='create'),
+    # 아래는 각 페이지마다 특정 pk값을 가진 놈들 넘겨주기 위함
     path('<int:pk>/detail/', views.detail,name='detail'),
     path('<int:pk>/update/', views.update,name='update'),
     path('<int:pk>/delete/', views.delete,name='delete'),
--- instagram/articles/views.py
+++ instagram/articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import ArticleForm
+from .forms import ArticleForm 
 from .models import Article
 
 # Create your views here.
