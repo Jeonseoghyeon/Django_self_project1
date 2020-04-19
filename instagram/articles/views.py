7cd99e4a6a35193c5f44831db648c519b2aa968c
Jeonseoghyeon <jsh28592@gmail.com>
2020-04-19 13:14:13 +0000

    articles-viws.py 주석

--- instagram/articles/views.py
+++ instagram/articles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from .forms import ArticleForm 
+from .forms import ArticleForm
 from .models import Article
+
+# 밑의 내용들은
 
 # Create your views here.
 def index(request):
@@ -11,7 +13,7 @@ def index(request):
     return render(request,'articles/index.html',context)
 
 def detail(request,pk):
-    article = Article.objects.get(pk=pk)
+    article = Article.objects.get(pk=pk) # pk, id 둘 다 상관 없음(일단 지금 상황에서는 같음)
     context = {
         'article':article
     }
@@ -36,9 +38,9 @@ def update(request,pk):
         form = ArticleForm(request.POST, instance=article)
         if form.is_valid():
             form.save()
-            return redirect('articles:detail', pk)
+            return redirect('articles:detail', pk) # redirect를 위해 pk값을 같이 넘겨준다 !!!!!!!!!!!
     else:
-        form = ArticleForm(instance=article)
+        form = ArticleForm(instance=article) # instance : 기존에 있던 값을 넣어주는 것 ! 수정해야 하니까 !!
     context = {
         'form':form
     }
