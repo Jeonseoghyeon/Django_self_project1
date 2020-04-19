397ef97b99de264d4b439091ead3f46f3216f21b
Jeonseoghyeon <jsh28592@gmail.com>
2020-04-19 13:11:48 +0000

    articles-forms.py 주석

--- instagram/articles/forms.py
+++ instagram/articles/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Article
 
+# model을 사용자들에게 보기좋게 만들어주기 위해 쓰는게 ModelForm!!! (선택사항이지만 안 할 필요는 없다.)
+
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
--- instagram/articles/models.py
+++ instagram/articles/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# models 자체는 데이터를 관리하기 위해 사용!
+# models은 는 데이터를 저장, 관리하기 위해 사용!
 # 그렇기 때문에 model class를 정의한 후에는 migrate 작업이 요구된다!
 
 # Create your models here.
