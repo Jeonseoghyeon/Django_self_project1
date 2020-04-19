080623ac635b65b6e5c9acdc6fd8d19f6177d55e
Jeonseoghyeon <jsh28592@gmail.com>
2020-04-19 12:16:12 +0000

    accounts-urls.py 주석

--- instagram/accounts/urls.py
+++ instagram/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 
-app_name = 'accounts'
+app_name = 'accounts' # app_name 추가 잊지 말자! 이걸 해줘야 'accounts:index'처럼 연결해줄 수 있음
 
 urlpatterns = [
+    # 각각 urlnamespace 써줬다 -> 나중에 세부 주소가 바뀌어도 처리해주기 쉬워짐!!!! * 이해한 부분!
     path('',views.index,name='index'),
     path('login/',views.login,name='login'),
     path('logout/',views.logout,name='logout'),
